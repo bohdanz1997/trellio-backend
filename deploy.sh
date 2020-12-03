1cf01c69ac9a16076fa45cde7cdbfc628434a08b
dev <dev@mail.com>
2020-12-03 12:02:00 +0200

    ci

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
 # Build docker image
-docker pull "$NODE_APP_IMAGE":staging
-docker build -t "$NODE_APP_IMAGE":$APP_VERSION_TAG .
+docker pull "$NODE_APP_IMAGE":"$APP_VERSION_TAG"
+docker build -t "$NODE_APP_IMAGE":"$APP_VERSION_TAG" .
 docker push "$NODE_APP_IMAGE":"$APP_VERSION_TAG"
 
 # Pull docker image to the server
