ef0adea6c840dada3897899d6b810d16369096e4
dev <dev@mail.com>
2020-12-04 10:43:02 +0200

    add ssh fingerprint

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-#apk add --no-cache --update openssh
-#pip3 install awscli
-#aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-#aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-#
-## Build docker image
-#docker login -u "$DOCKERHUB_USER" -p "$DOCKERHUB_PASSWORD"
-#
-#docker pull "$NODE_APP_IMAGE":"$APP_VERSION_TAG" || true
-#docker build -t "$NODE_APP_IMAGE":"$APP_VERSION_TAG" .
-#docker push "$NODE_APP_IMAGE":"$APP_VERSION_TAG"
+apk add --no-cache --update openssh
+pip3 install awscli
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
-git pull origin
+# Build docker image
+docker login -u "$DOCKERHUB_USER" -p "$DOCKERHUB_PASSWORD"
+
+docker pull "$NODE_APP_IMAGE":"$APP_VERSION_TAG" || true
+docker build -t "$NODE_APP_IMAGE":"$APP_VERSION_TAG" .
+docker push "$NODE_APP_IMAGE":"$APP_VERSION_TAG"
 
 # Pull docker image to the server
 ssh-keyscan -H "$SSH_SERVER" >> ~/.ssh/known_hosts
-ssh "$SSH_USER"@"$SSH_SERVER" "cd /home/ubuntu/app; AWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID' AWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY' ./pull-and-up-images.sh"
+ssh "$SSH_USER"@"$SSH_SERVER" "cd /home/ubuntu/app; git pull origin && AWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID' AWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY' ./pull-and-up-images.sh"
